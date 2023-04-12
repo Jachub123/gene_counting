fc8f8a0c8ea110b2f3a02e11b82772ad9a594b6e
jachub123 <jakobnonnenmacher@hotmail.com>
2023-04-12 16:41:07 +0200

    added paypal and comment section *fixed error

--- pages/index.js
+++ pages/index.js
@@ -1011,7 +1011,7 @@ export default function Home() {
       </div>
 
       <Analytics />
-      <div class="powr-form-builder" id="18a39f72_1681306315"></div>
+      <div className="powr-form-builder" id="18a39f72_1681306315"></div>
       <script src="https://www.powr.io/powr.js?platform=html"></script>
       <form className="donate" action="https://www.paypal.com/donate" method="post" target="_top">
         <input type="hidden" name="hosted_button_id" value="YSS2VHFZ8FPC2" />
